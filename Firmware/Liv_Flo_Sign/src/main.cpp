f1c9f44d876f3ba8931ef40e8dbbccee6af0cf38
Florian Baumgartner <florian.b-h@bluewin.ch>
2024-10-03 23:35:32 -0500

    v0.0.23

--- Firmware/Liv_Flo_Sign/src/main.cpp
+++ Firmware/Liv_Flo_Sign/src/main.cpp
@@ -98,7 +98,7 @@ void setup()
   matrix.setTextSize(1);
   matrix.setTextWrap(false);
   matrix.setBrightness(3);
-  matrix.setTextColor(matrix.Color(0, 0, 255));
+  matrix.setTextColor(matrix.Color(0, 255, 0));
 
   githubOTA.begin(REPO_URL);
   xTaskCreate(updateTask, "main_task", 4096, NULL, 20, NULL);
